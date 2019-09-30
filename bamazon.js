82db8c1d279505f9c744420064948e5ed34b77a5
JeffM <jeff@jeffreymclean.com>
2019-09-29 19:28:36 -0700

    started adding inquirer prompts. Made Item IDs into their own array, for validating in inquirer

--- bamazon.js
+++ bamazon.js
@@ -3,40 +3,60 @@ var inquirer = require("inquirer");
 var connectionInfo = require("./connection_info");
 var mysql = require("mysql");
 var connection = mysql.createConnection(connectionInfo.connection);
-
+var itemIDs =[];
 
 // --------------- CONNECTS TO THE BAMAZON DATABASE & INITIALIZES APP -------------
 connection.connect(function (err) {
-   if (err) throw err;
+    if (err) throw err;
     console.log("Connected to Bamazon (Database ID: " + connection.threadId + ")\n");
     showProductTable();
-    
+
 });
 
 // -------------------- FUNCTION TO SHOW PRODUCT TABLE -------------------
-function showProductTable(){
-   connection.query("SELECT * FROM products", function (err, res){
+function showProductTable() {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log ( "<------------------ ITEMS FOR SALE ------------------------>")
-        console.table(res); 
 
+        // ------------ Used for Validation in Inquirer ------------
+        for (var i = 0; i < res.length; i++){
+           itemIDs.push(res[i].item_id);
+        }
+        // ----------------------------------------------------------
+
+        items = res;
+        console.log("<------------------ ITEMS FOR SALE ------------------------>")
+        console.table(res);
         customerAction();
     });
+
+    
 };
 
 
-function customerAction (){
+function customerAction() {
     inquirer.prompt([
         {
             type: "number",
             name: "item_selected",
-            message: "Which Item Would You Like To Purchase? [Enter Item ID, Example: 2]"
+            message: "Which Item Would You Like To Purchase? [Enter Item ID, Example: 2 for 'Roku 4']",
+            validate: function (value) {
+                if (itemIDs.includes(value)) {
+                    return true;
+                }
+                else {
+                    console.log("\n ------Please Select A Valid Item------")
+                    return customerAction()
+                }
+
+            }
         },
         {
             type: "number",
             name: "item_quantity",
             message: "How Many Would You Like To Buy?"
-        },
+        }
+    ]).then(function (answer) {
 
-    ])
+    })
 }
