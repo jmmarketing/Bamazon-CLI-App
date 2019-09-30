39513bbfbe9bd62d078e5d3d1e7a865e6c243b01
JeffM <jeff@jeffreymclean.com>
2019-09-29 18:11:15 -0700

    Added Function to Populate Table of Items, Initiates database connection

--- bamazon.js
+++ bamazon.js
@@ -4,7 +4,39 @@ var connectionInfo = require("./connection_info");
 var mysql = require("mysql");
 var connection = mysql.createConnection(connectionInfo.connection);
 
+
+// --------------- CONNECTS TO THE BAMAZON DATABASE & INITIALIZES APP -------------
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to Bamazon (Database ID: " + connection.threadId + ").\n");
+   if (err) throw err;
+    console.log("Connected to Bamazon (Database ID: " + connection.threadId + ")\n");
+    showProductTable();
+    
 });
+
+// -------------------- FUNCTION TO SHOW PRODUCT TABLE -------------------
+function showProductTable(){
+   connection.query("SELECT * FROM products", function (err, res){
+        if (err) throw err;
+        console.log ( "<------------------ ITEMS FOR SALE ------------------------>")
+        console.table(res); 
+
+        customerAction();
+    });
+};
+
+
+function customerAction (){
+    inquirer.prompt([
+        {
+            type: "number",
+            name: "item_selected",
+            message: "Which Item Would You Like To Purchase? [Enter Item ID, Example: 2]"
+        },
+        {
+            type: "number",
+            name: "item_quantity",
+            message: "How Many Would You Like To Buy?"
+        },
+
+    ])
+}
